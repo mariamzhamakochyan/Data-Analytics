ed8df5f0cb77e3d0599ff34cbceaa1b2f1b2e608
mariamzhamakochyan <mariamzhamakochyan@gmail.com>
2023-02-13 19:57:43 +0400

    goldbach function done

--- 13.02/goldbach.py
+++ 13.02/goldbach.py
@@ -1,1 +1,16 @@
-.
+def is_prime(num):
+    for i in range(2, num // 2):
+        if num % i == 0:
+            return False
+    return True
+
+def goldbach(n):
+    if n > 2 and n % 2 == 0:
+        for n1 in range(2, n // 2):
+            n2 = n - n1
+            if is_prime(n1) and is_prime(n2):
+                return [n1, n2]
+    else:
+        return "Number must be even and greater than two."
+n = int(input("Enter a number: "))
+print(goldbach(n))
